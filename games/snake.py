ea9d6202889f74a8760b094a0157ca8d2be49035
l0drex <sunshine.lorenz@web.de>
2020-07-28 11:34:27 +0200

    More fixes

--- games/snake.py
+++ games/snake.py
@@ -2,14 +2,14 @@ import curses
 from typing import Dict
 from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
 from random import randint
-from .game import Game
+from .uigame import UiGame
 
 
-class Snake(Game):
+class Snake(UiGame):
 
     def __init__(self):
         # its a game
-        super(Game, self).__init__()
+        super(UiGame, self).__init__()
 
         self.info: Dict = {
             'name': 'Snake',
@@ -20,14 +20,6 @@ class Snake(Game):
         self.running: bool = True
 
     def gameround(self, players) -> int:
-        curses.initscr()
-        win = curses.newwin(20, 60, 0, 0)
-        win.keypad(1)
-        curses.noecho()
-        curses.curs_set(0)
-        win.border(0)
-        win.nodelay(1)
-
         # Initializing values
         self.key = KEY_RIGHT
         self.score = 0
@@ -38,21 +30,21 @@ class Snake(Game):
         food = [10, 20]
 
         # Prints the food
-        win.addch(food[0], food[1], '*')
+        self.win.addch(food[0], food[1], '*')
 
         # While Esc key is not pressed
         while (self.key != 27):
-            win.border(0)
+            self.win.border(0)
             # Printing 'Score' and
-            win.addstr(0, 2, 'Score : ' + str(self.score) + ' ')
+            self.win.addstr(0, 2, 'Score : ' + str(self.score) + ' ')
             # 'SNAKE' strings
-            win.addstr(0, 27, ' SNAKE ')
+            self.win.addstr(0, 27, ' SNAKE ')
             # Increases the speed of Snake as its length increases
-            win.timeout(int(150 - (len(snake)/5 + len(snake)/10) % 120))
+            self.win.timeout(int(150 - (len(snake)/5 + len(snake)/10) % 120))
 
             # Previous key pressed
             prevKey = self.key
-            event = win.getch()
+            event = self.win.getch()
             self.key = self.key if event == -1 else event
 
             # If SPACE BAR is pressed, wait for another
@@ -60,7 +52,7 @@ class Snake(Game):
                 # one (Pause/Resume)
                 self.key = -1
                 while self.key != ord(' '):
-                    self.key = win.getch()
+                    self.key = self.win.getch()
                 self.key = prevKey
                 continue
 
@@ -83,17 +75,6 @@ class Snake(Game):
                 y += 1
             snake.insert(0, [x, y])
 
-            # If snake crosses the boundaries,
-            # make it enter from the other side
-            if snake[0][0] == 0:
-                snake[0][0] = 18
-            if snake[0][1] == 0:
-                snake[0][1] = 58
-            if snake[0][0] == 19:
-                snake[0][0] = 1
-            if snake[0][1] == 59:
-                snake[0][1] = 1
-
             # Exit if snake crosses the boundaries (Uncomment to enable)
             if(snake[0][0] == 0 or snake[0][0] == 19 or
                snake[0][1] == 0 or snake[0][1] == 59):
@@ -112,12 +93,13 @@ class Snake(Game):
                     food = [randint(1, 18), randint(1, 58)]
                     if food in snake:
                         food = []
-                win.addch(food[0], food[1], '*')
+                self.win.addch(food[0], food[1], '*')
             else:
                 # [1] If it does not eat the food, length decreases
                 last = snake.pop()
-                win.addch(last[0], last[1], ' ')
-            win.addch(snake[0][0], snake[0][1], '#')
+                self.win.addch(last[0], last[1], ' ')
+            self.win.addch(snake[0][0], snake[0][1], '#')
+        self.running = False
 
     def end(self):
         if not self.running:
