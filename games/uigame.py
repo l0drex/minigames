58abd310349d6ac3a3e5709090a9f3ebcc221b85
l0drex <sunshine.lorenz@web.de>
2020-07-28 11:33:06 +0200

    Add uigame

---
+++ games/uigame.py
@@ -0,0 +1,25 @@
+import curses
+from .game import Game
+
+
+class UiGame(Game):
+    def __init__(self):
+        # its a game
+        super(Game, self).__init__()
+
+        self.words = self.get_wordlist()
+
+    def start(self):
+        """
+        What happens before the first round?
+        """
+        print('Willkommen bei ' + self.get_info('name'))
+        print(self.get_info('description'))
+        print('\n\n' + input())
+        curses.initscr()
+        self.win = curses.newwin(20, 60, 0, 0)
+        self.win.keypad(1)
+        curses.noecho()
+        curses.curs_set(0)
+        self.win.border(1)
+        self.win.nodelay(1)
